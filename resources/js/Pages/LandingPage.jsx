7ba963de334c1edc3bb7828afb4408a6d486bcde
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-06-30 19:08:46 +0800

    Created

---
+++ resources/js/Pages/LandingPage.jsx
@@ -0,0 +1,10 @@
+import { Link, Head } from "@inertiajs/react";
+
+export default function LandingPage() {
+    return (
+        <>
+            <Head title="Welcome" />
+            <div className="landing-page-container"></div>
+        </>
+    );
+}
