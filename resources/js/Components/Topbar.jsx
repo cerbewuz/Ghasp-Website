b29fb51654aab7cd4939bbe14854699d5ee28e92
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-06-30 19:20:17 +0800

    created a design for topbar

--- resources/js/Components/Topbar.jsx
+++ resources/js/Components/Topbar.jsx
@@ -1,7 +1,12 @@
+import Logo from "@/Components/Logo";
 export default function Topbar() {
     return (
         <>
-            <div className="topbar-container"></div>
+            <div className="topbar-container">
+                <div className="topbar-container-inside">
+                    <Logo />
+                </div>
+            </div>
         </>
     );
 }
