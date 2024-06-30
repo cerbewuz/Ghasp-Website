a4113d43206592a7e202658c38dd20d449f6a4d4
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-06-30 19:30:19 +0800

    added a prop to map over the items of the menu

--- resources/js/Components/Menu.jsx
+++ resources/js/Components/Menu.jsx
@@ -1,8 +1,17 @@
-export default function Menu() {
+export default function Menu({ items }) {
     return (
         <>
             <div className="menu-container">
-                
+                <div className="menu-coontainer-inside">
+                    {items.map((item, index) => (
+                        <span
+                            key={index}
+                            className="menu-container-inside-item"
+                        >
+                            {item}
+                        </span>
+                    ))}
+                </div>
             </div>
         </>
     );
--- resources/js/Components/Topbar.jsx
+++ resources/js/Components/Topbar.jsx
@@ -1,10 +1,20 @@
 import Logo from "@/Components/Logo";
+import Menu from "@/Components/Menu";
 export default function Topbar() {
     return (
         <>
             <div className="topbar-container">
                 <div className="topbar-container-inside">
                     <Logo />
+                    <Menu
+                        items={[
+                            "Home",
+                            "Gallery",
+                            "About Us",
+                            "Login",
+                            "Contact Us",
+                        ]}
+                    />
                 </div>
             </div>
         </>
