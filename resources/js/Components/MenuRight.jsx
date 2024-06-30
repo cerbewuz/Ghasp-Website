9a49f0348210270550bec5631c594be03497f308
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-06-30 22:47:36 +0800

    added router for the sign in

--- resources/js/Components/MenuRight.jsx
+++ resources/js/Components/MenuRight.jsx
@@ -1,10 +1,19 @@
+import { router } from "@inertiajs/react";
+
 export default function MenuRight({ items }) {
+    const navigate = () => {
+        router.visit("/login");
+    };
     return (
         <>
             <div className="menu-container">
                 <div className="menu-container-inside">
                     {items.map((item, index) => (
-                        <div key={index} className="menu-container-inside-item">
+                        <div
+                            key={index}
+                            className="menu-container-inside-item"
+                            onClick={navigate}
+                        >
                             <span>{item}</span>
                         </div>
                     ))}
