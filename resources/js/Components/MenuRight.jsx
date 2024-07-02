91a9af60d7b24471335ff7c2447706447aba4315
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-02 23:35:11 +0800

    Updated the bg color

--- resources/js/Components/MenuRight.jsx
+++ resources/js/Components/MenuRight.jsx
@@ -1,8 +1,28 @@
 import { router } from "@inertiajs/react";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 export default function MenuRight({ items }) {
     const [inputSearch, setInputSearch] = useState(false);
+
+    const searchRef = useRef(null);
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (
+                searchRef.current &&
+                !searchRef.current.contains(event.target)
+            ) {
+                setInputSearch(false);
+            }
+        }
+
+        // Bind the event listener
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            // Unbind the event listener on clean up
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
     const navigate = () => {
         router.visit("/login");
     };
@@ -10,7 +30,7 @@ export default function MenuRight({ items }) {
         <>
             <div className="menu-container">
                 <div className="menu-container-inside">
-                    <div className="search-container">
+                    <div className="search-container" ref={searchRef}>
                         <input
                             type="text"
                             className={`input-item-search ${
