13bad8e99656beb79f57d4b45d1acb40394a2cac
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-02 23:14:56 +0800

    adding a state for opening search bar

--- resources/js/Components/MenuRight.jsx
+++ resources/js/Components/MenuRight.jsx
@@ -1,6 +1,8 @@
 import { router } from "@inertiajs/react";
-
+import { useState } from "react";
+import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
 export default function MenuRight({ items }) {
+    const [inputSearch, setInputSearch] = useState(false);
     const navigate = () => {
         router.visit("/login");
     };
@@ -8,6 +10,20 @@ export default function MenuRight({ items }) {
         <>
             <div className="menu-container">
                 <div className="menu-container-inside">
+                    <div
+                        className="search-container"
+                        onClick={setInputSearch(!inputSearch)}
+                    >
+                        <input
+                            type="text"
+                            className={`input-item-search ${
+                                inputSearch ? "display-option" : ""
+                            }`}
+                        />
+                        <div className="icon-container">
+                            <SearchRoundedIcon />
+                        </div>
+                    </div>
                     {items.map((item, index) => (
                         <div
                             key={index}
