49faf8c21e46d862bdea12d049fbde742e947035
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-07-16 08:36:29 +0800

    Update Login.jsx

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -1,38 +1,164 @@
-import { useEffect } from 'react';
-import Checkbox from '@/Components/Checkbox';
-import GuestLayout from '@/Layouts/GuestLayout';
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
-import { Head, Link, useForm } from '@inertiajs/react';
+import { useEffect } from "react";
+import Checkbox from "@/Components/Checkbox";
+import GuestLayout from "@/Layouts/GuestLayout";
+import InputError from "@/Components/InputError";
+import InputLabel from "@/Components/InputLabel";
+import PrimaryButton from "@/Components/PrimaryButton";
+import TextInput from "@/Components/TextInput";
+import { Head, Link, useForm } from "@inertiajs/react";
+import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
 
 export default function Login({ status, canResetPassword }) {
     const { data, setData, post, processing, errors, reset } = useForm({
-        email: '',
-        password: '',
+        email: "",
+        password: "",
         remember: false,
     });
 
     useEffect(() => {
         return () => {
-            reset('password');
+            reset("password");
         };
     }, []);
 
     const submit = (e) => {
         e.preventDefault();
 
-        post(route('login'));
+        post(route("login"));
     };
 
     return (
         <GuestLayout>
+            {/* Visual indicator for loading  */}
+
             <Head title="Log in" />
 
-            {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
+            {status && (
+                <div className="mb-4 font-medium text-sm text-green-600">
+                    {status}
+                </div>
+            )}
 
-            <form onSubmit={submit}>
+            {/* Start of the card.. */}
+            <div className="main-guest">
+                <div className="main-guest__card">
+                    <div className="main-guest__card-flex">
+                        <div className="main-guest__card-head">
+                            <h1 style={{ fontSize: 20 }}>Logo Here!</h1>
+                        </div>
+                        <div className="main-guest__card-body">
+                            <div className="card-body__item top">
+                                <h1 style={{ fontSize: 22 }}>Sign in</h1>
+                                <h2 style={{ fontSize: 15, fontWeight: 400 }}>
+                                    Continue to *name of the app*
+                                </h2>
+                            </div>
+                            <div className="card-body__item content">
+                                <div className="card-body-item__input">
+                                    <label className="label-item" htmlFor="">
+                                        Email
+                                    </label>
+                                    <input className="input-item" type="text" />
+                                </div>
+                                <div className="card-body-item__button">
+                                    <button className="card-body-item__button-item">
+                                        Continue with email
+                                    </button>
+                                </div>
+                                <div className="card-body-item__divide">
+                                    <span className="divide__or">or</span>
+                                </div>
+                                <div className="card-body-item__medias">
+                                    <div className="medias-icon__item">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            x="0px"
+                                            y="0px"
+                                            width="30"
+                                            height="32"
+                                            viewBox="0 0 48 48"
+                                        >
+                                            <linearGradient
+                                                id="Ld6sqrtcxMyckEl6xeDdMa_uLWV5A9vXIPu_gr1"
+                                                x1="9.993"
+                                                x2="40.615"
+                                                y1="9.993"
+                                                y2="40.615"
+                                                gradientUnits="userSpaceOnUse"
+                                            >
+                                                <stop
+                                                    offset="0"
+                                                    stop-color="#2aa4f4"
+                                                ></stop>
+                                                <stop
+                                                    offset="1"
+                                                    stop-color="#007ad9"
+                                                ></stop>
+                                            </linearGradient>
+                                            <path
+                                                fill="url(#Ld6sqrtcxMyckEl6xeDdMa_uLWV5A9vXIPu_gr1)"
+                                                d="M24,4C12.954,4,4,12.954,4,24s8.954,20,20,20s20-8.954,20-20S35.046,4,24,4z"
+                                            ></path>
+                                            <path
+                                                fill="#fff"
+                                                d="M26.707,29.301h5.176l0.813-5.258h-5.989v-2.874c0-2.184,0.714-4.121,2.757-4.121h3.283V12.46 c-0.577-0.078-1.797-0.248-4.102-0.248c-4.814,0-7.636,2.542-7.636,8.334v3.498H16.06v5.258h4.948v14.452 C21.988,43.9,22.981,44,24,44c0.921,0,1.82-0.084,2.707-0.204V29.301z"
+                                            ></path>
+                                        </svg>
+                                    </div>
+                                    <div className="medias-icon__item">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            x="0px"
+                                            y="0px"
+                                            width="30"
+                                            height="28"
+                                            viewBox="0 0 48 48"
+                                        >
+                                            <path
+                                                fill="#FFC107"
+                                                d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"
+                                            ></path>
+                                            <path
+                                                fill="#FF3D00"
+                                                d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"
+                                            ></path>
+                                            <path
+                                                fill="#4CAF50"
+                                                d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"
+                                            ></path>
+                                            <path
+                                                fill="#1976D2"
+                                                d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"
+                                            ></path>
+                                        </svg>
+                                    </div>
+                                </div>
+                                <div className="card-body-item__redirects">
+                                    <span className="redirect-text">
+                                        New to *app*?
+                                    </span>
+                                    <Link
+                                        href={route("register")}
+                                        className="redirect-link"
+                                    >
+                                        Get started
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="main-guest__card-footer">
+                            <div className="card-footer__links">
+                                <span className="card-footer__link">Help</span>
+                                <span className="card-footer__link">
+                                    Privacy
+                                </span>
+                                <span className="card-footer__link">Terms</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {/* <form onSubmit={submit}>
                 <div>
                     <InputLabel htmlFor="email" value="Email" />
 
@@ -44,7 +170,7 @@ export default function Login({ status, canResetPassword }) {
                         className="mt-1 block w-full"
                         autoComplete="username"
                         isFocused={true}
-                        onChange={(e) => setData('email', e.target.value)}
+                        onChange={(e) => setData("email", e.target.value)}
                     />
 
                     <InputError message={errors.email} className="mt-2" />
@@ -60,7 +186,7 @@ export default function Login({ status, canResetPassword }) {
                         value={data.password}
                         className="mt-1 block w-full"
                         autoComplete="current-password"
-                        onChange={(e) => setData('password', e.target.value)}
+                        onChange={(e) => setData("password", e.target.value)}
                     />
 
                     <InputError message={errors.password} className="mt-2" />
@@ -71,16 +197,20 @@ export default function Login({ status, canResetPassword }) {
                         <Checkbox
                             name="remember"
                             checked={data.remember}
-                            onChange={(e) => setData('remember', e.target.checked)}
+                            onChange={(e) =>
+                                setData("remember", e.target.checked)
+                            }
                         />
-                        <span className="ms-2 text-sm text-gray-600 dark:text-gray-400">Remember me</span>
+                        <span className="ms-2 text-sm text-gray-600 dark:text-gray-400">
+                            Remember me
+                        </span>
                     </label>
                 </div>
 
                 <div className="flex items-center justify-end mt-4">
                     {canResetPassword && (
                         <Link
-                            href={route('password.request')}
+                            href={route("password.request")}
                             className="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
                         >
                             Forgot your password?
@@ -91,7 +221,7 @@ export default function Login({ status, canResetPassword }) {
                         Log in
                     </PrimaryButton>
                 </div>
-            </form>
+            </form> */}
         </GuestLayout>
     );
 }
